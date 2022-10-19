f6e6475f1e245f94833e7674c35c3a36c57c0b09
Olabode1232 <olukanstephen.olabode@gmail.com>
2022-10-19 13:55:41 +0100

    done fetching the movies API and handling of error

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import MoviesList from "./component/moviesList";
 import "./app.css";
 
-const App = () => {
-  const dummy_Movies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      subtitle: "This is the opening text of the movie",
-      releaseDate: "2022,05,12"
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      subtitle: "This is the second opening text of the movie",
-      releaseDate: "2022,08,22"
-    },
-  ];
+function App () {
+  const [moviee, setMoviee] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  async function fetchMovieHandler() {
+    setIsLoading(true)
+    setError(null)
+    try{
+      const response = await fetch("https://swapi.dev/api/films/");
+      if(!response.ok){
+        throw new Error("something went wrong")
+      }
+      const data = await response.json();
+    
+      const transformedMoviee = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          subtitle: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMoviee(transformedMoviee)
+    } catch(error) {
+       setError(error.message)
+    }
+    setIsLoading(false)
+    
+  }
   return (
-    <div className="container-fluid bg-dark mt-2">
+    <div className="container-fluid bg-dark mt-2 text-center">
       <div className="row justify-content-center mb-3">
         <div className="col-md-8 bg-light rounded-3 text-center">
-        <button className="btn-outline-primary m-3 rounded-2">Fetch Movies</button>
-
+          <button
+            className="btn-outline-primary m-3 rounded-2"
+            onClick={fetchMovieHandler}
+          >
+            Fetch Movies
+          </button>
         </div>
       </div>
       <div className="row justify-content-center">
         <div className="col-md-8 bg-light rounded-3">
-          <MoviesList movies={dummy_Movies} />
+          {!isLoading && moviee.length > 0 && <MoviesList movies={moviee} />}
+          {isLoading &&  <p>Loading...</p>}
+          {!isLoading && moviee.length === 0 && !error && <p>Found no movies</p>}
+          {error && <p>{error}</p>}
         </div>
       </div>
     </div>
--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -6,8 +6,9 @@ const Movies = (props) => {
       <div className="row">
         <div className="col-md-10 m-4">
           <li className="list-unstyled bg-dark p-4 text-center rounded-3">
-          <h1 className="text-warning">{props.title}</h1>
-          <h5 className="text-light">{props.subtitle}</h5>
+            <h1 className="text-warning">{props.title}</h1>
+            <h3>{props.releaseDate}</h3>
+            <p className="text-light">{props.subtitle}</p>
           </li>
         </div>
       </div>
